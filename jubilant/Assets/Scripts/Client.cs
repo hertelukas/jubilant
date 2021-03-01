51509224fb9b37ea43a28faa5a279b610bc0a7c4
hertelukas <hertelukas@users.noreply.github.com>
2021-03-01 16:38:37 +0100

    Using UTF8 encoding

--- jubilant/Assets/Scripts/Client.cs
+++ jubilant/Assets/Scripts/Client.cs
@@ -127,7 +127,7 @@ public class Client : MonoBehaviour
             if (bytesRead > 0)
             {
                 // There might be more data, so store the data received so far.  
-                state.sb.Append(Encoding.ASCII.GetString(state.buffer, 0, bytesRead));
+                state.sb.Append(Encoding.UTF8.GetString(state.buffer, 0, bytesRead));
 
                 // Get the rest of the data.  
                 client.BeginReceive(state.buffer, 0, StateObject.BufferSize, 0,
@@ -153,7 +153,7 @@ public class Client : MonoBehaviour
     private static void Send(Socket client, String data)
     {
         // Convert the string data to byte data using ASCII encoding.  
-        byte[] byteData = Encoding.ASCII.GetBytes(data);
+        byte[] byteData = Encoding.UTF8.GetBytes(data);
 
         // Begin sending the data to the remote device.  
         client.BeginSend(byteData, 0, byteData.Length, 0,
